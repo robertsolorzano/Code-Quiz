1ed1e7700a707360d800876a0fe59d92ddf4bf27
Robert Solorzano <robertsolorzano510@gmail.com>
2023-12-06 10:16:09 -0800

    testing event listener beging moving forward

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var startButton = document.getElementById('startButton');
 
 //Add Event listener
 startButton.addEventListener('click', function() {
+console.log('Game has commnenced')
 
 
 
 
-    
 })
